d26ccb34a0dbfd74da3fc86f132cbb76745e9b9c
OlgaZdanovich <erochenko92@mail.ru>
2022-09-26 00:41:35 +0300

    Task 3

--- Script.java
+++ Script.java
@@ -1,36 +1,17 @@
-/**
- * Тривиальная (1 балл)
- * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
- * Например, расстояние между (3, 0) и (0, 4) равно 5
+/**Простая (2 балла)
+ * Пользователь задает целое число, больше или равно 100 (например, 3801).
+ * Определить третью цифру справа в этом числе (в данном случае 8).
  */
 import java.util.Scanner;
 public class Script {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите координаты первой точки x,y через \",\"\n");
-        String InputStr1 = in.nextLine();
+        System.out.print("Введите целое число >= 100\n");
+        int InputNumber = in.nextInt();
+        int NewNumber = (InputNumber / 100) % 10;
 
-
-        System.out.print("Введите координаты второй точки x,y через \",\"\n");
-        String InputStr2 = in.nextLine();
-
-        String[] StrLine1 = InputStr1.split(",");
-        String x1Str = StrLine1[0];
-        String y1Str = StrLine1[1];
-
-        String[] StrLine2 = InputStr2.split(",");
-        String x2Str = StrLine2[0];
-        String y2Str = StrLine2[1];
-
-        double y1 = Double.parseDouble (y1Str);
-        double x1 = Double.parseDouble (x1Str);
-
-        double y2 = Double.parseDouble (y2Str);
-        double x2 = Double.parseDouble (x2Str);
-
-        double LengthAB = Math.sqrt(((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)));
-        System.out.printf("Расстояние между точками: %f\n",LengthAB);
+        System.out.printf("Третья цифра справа: %d\n",NewNumber);
 
         in.close();
     }
